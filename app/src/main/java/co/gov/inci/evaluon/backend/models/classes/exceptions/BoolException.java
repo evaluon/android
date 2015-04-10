ed5d08cbca15e182cceb804a29741f5ccd4512a0
William Suárez <suarezwilliamf@gmail.com>
2015-04-10 15:49:31 -0500

    help layout

--- app/src/main/java/co/gov/inci/evaluon/backend/models/classes/exceptions/BoolException.java
+++ app/src/main/java/co/gov/inci/evaluon/backend/models/classes/exceptions/BoolException.java
@@ -20,6 +20,28 @@ import co.gov.inci.evaluon.R;
         put("no_active_period", R.string.error_no_active_period);
         put("no_active_test", R.string.error_no_active_tests);
         put("invalid_hotp_code", R.string.error_invalid_hotp_code);
+        put("is_evaluee", R.string.error_is_evaluee);
+        put("invalid_permissions", R.string.error_invalid_permissions);
+        put("not_an_evaluee", R.string.error_not_an_evaluee);
+        put("not_evaluees", R.string.error_not_evaluees);
+        put("test_unopened", R.string.error_test_unopened);
+        put("unabled_institution", R.string.error_unabled_institution);
+        put("insuficient_privileges", R.string.error_insuficient_privileges);
+        put("invalid_grant", R.string.error_invalid_grant);
+        put("access_denied", R.string.error_access_denied);
+        put("already_opened_test", R.string.error_already_opened_test);
+        put("existing_user", R.string.error_existing_user);
+        put("test_unavailable", R.string.error_test_unavailable);
+        put("client_not_found", R.string.error_client_not_found);
+        put("no_questions_available", R.string.error_no_questions_available);
+
+
+
+
+
+
+
+
     }};
 
     String code;
